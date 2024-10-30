37450eb72c441ff1fde38008d4e324baa37947cd
Kaan Karakus <kaankarakus897@gmail.com>
2024-10-30 23:18:39 +0300

    feat : add image controller

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,11 +1,90 @@
+import { NextFunction, Request, Response } from "express";
+import imageService from "../services/image.service";
+
 class ImageController {
-  async uploadImage() {}
+  async uploadImage(req: Request, res: Response, next: NextFunction) {
+    try {
+      const file = req.file;
+      if (!file) {
+        res.status(400).json({ message: "Please upload an image" });
+        return;
+      }
 
-  async transformImage() {}
+      const userId = req.user?.id as number;
 
-  async getImage() {}
+      const data = await imageService.uploadImage(
+        file,
+        req.body.metadata,
+        userId
+      );
 
-  async getImages() {}
+      res.status(201).json({
+        success: true,
+        data,
+      });
+    } catch (error: unknown) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  async transformImage(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { transformations } = req.body;
+      const userId = req.user?.id as number;
+
+      await imageService.transformImage(Number(id), transformations, userId);
+
+      res.status(200).json({
+        success: true,
+        message: "Image transformed successfully",
+      });
+    } catch (error: unknown) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  async getImage(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+
+      const userId = req.user?.id as number;
+
+      const image = await imageService.getImage(Number(id), userId);
+
+      res.status(200).json({
+        success: true,
+        data: image,
+      });
+    } catch (error: unknown) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  async getImages(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { page, limit } = req.query;
+
+      const userId = req.user?.id as number;
+
+      const images = await imageService.getImages(
+        Number(page),
+        Number(limit),
+        userId
+      );
+
+      res.status(200).json({
+        success: true,
+        data: images,
+      });
+    } catch (error: unknown) {
+      console.log(error);
+      next(error);
+    }
+  }
 }
 
 export default new ImageController();
