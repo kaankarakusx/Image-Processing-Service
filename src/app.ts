1b5f0527cb5a14b49c082b0c41ca045683071772
Kaan Karakus <kaankarakus897@gmail.com>
2024-10-30 23:17:35 +0300

    feat : update app

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,28 @@ import routes from "./routes/index";
 //connect to database
 import "./config/db";
 import { errorHandler } from "./middlewares/error.middleware";
+import path from "path";
+import { IUserPayload } from "./interfaces/IUserPayload";
+import rateLimit from "express-rate-limit";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: IUserPayload;
+  }
+}
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+app.use(limiter);
 app.use("/api/v1", routes);
 app.use(
   (
